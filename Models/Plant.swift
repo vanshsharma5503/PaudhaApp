bad33f66f0f2debd349d5c11c6faba93da6479be
Vansh sharma <91759509+vanshsharma5503@users.noreply.github.com>
2024-02-21 15:06:01 +0530

    Add files via upload

---
+++ Models/Plant.swift
@@ -0,0 +1,15 @@
+//
+//  Plant.swift
+//  PaudhaUI
+//
+//  Created by user1 on 19/02/24.
+//
+
+import SwiftUI
+
+struct Plant: Identifiable {
+    var id = UUID()
+        var imageName: String
+        var nickname: String
+        var type: String
+}
